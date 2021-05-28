4f0dfa45266dfe25c4cc262db08f3bcf80edc5ed
yun-jiyeong <70522500+jiyeong-yun@users.noreply.github.com>
2021-05-29 00:52:57 +0900

    지도 설정

--- MyCalendar/app/src/main/java/org/techtown/mycalendar/MapActivity.java
+++ MyCalendar/app/src/main/java/org/techtown/mycalendar/MapActivity.java
@@ -9,6 +9,7 @@ import android.graphics.PointF;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
+import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
@@ -20,62 +21,138 @@ import com.skt.Tmap.TMapPolyLine;
 import com.skt.Tmap.TMapView;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 public class MapActivity extends AppCompatActivity {
     Intent intent;
     int position;
     Handler handler;
 
+    TMapView tmapview;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
 
-        LinearLayout linearLayoutTmap = (LinearLayout)findViewById(R.id.linearLayoutTmap);
-        TMapView tmapview = new TMapView(this);
+        // 지도 띄우기
+        tmapview = new TMapView(this);
+        tmapview.setSKTMapApiKey("l7xxf07bcc5a789e4d678d5622e927b5e84a");
 
-        tmapview.setSKTMapApiKey( "l7xxf07bcc5a789e4d678d5622e927b5e84a" );
-        linearLayoutTmap.addView( tmapview );
+        initialize(tmapview);
 
-/*        tmapdata.findAllPOI(strData, new TMapData.FindAllPOIListenerCallback() {
-            @Override
-            public void onFindAllPOI(ArrayList poiItem) {
-                for(int i = 0; i < poiItem.size(); i++) {
-                    TMapPOIItem  item = (TMapPOIItem) poiItem.get(i);
-                    Log.d("POI Name: ", item.getPOIName().toString() + ", " +
-                            "Address: " + item.getPOIAddress().replace("null", "")  + ", " +
-                            "Point: " + item.getPOIPoint().toString());
-                }
-            }
-        });*/
+    }
 
-        // 리버스 지오코딩
-        try {
-            final String address = new TMapData().convertGpsToAddress(tmapview.getLatitude(), tmapview.getLongitude());
-            handler.post(new Runnable() {
+    private void initialize(TMapView tmapview) {
+        LinearLayout linearLayoutTmap = (LinearLayout) findViewById(R.id.linearLayoutTmap);
+        linearLayoutTmap.addView(tmapview);
+
+        // 전북대로 설정
+        tmapview.setOnClickListenerCallBack(mOnClickListenerCallback);
+        tmapview.setZoomLevel(15);
+        tmapview.setCenterPoint(127.129436, 35.846964);
+
+        // 마커표시
+        ArrayList<String> arrBuilding = new ArrayList<>();
+        arrBuilding.add("공과대학 7호관");
+
+        searchPOI(arrBuilding);
+
+    }
+
+    // 주변 명칭 검색
+    private void searchPOI(ArrayList<String> arrPOI)
+    {
+        final TMapData tMapData = new TMapData();
+        final ArrayList<TMapPoint> arrTMapPoint = new ArrayList<>();
+        final ArrayList<String> arrTitle = new ArrayList<>();
+        final ArrayList<String> arrAddress = new ArrayList<>();
+
+        for(int i = 0; i < arrPOI.size(); i++ )
+        {
+            tMapData.findTitlePOI(arrPOI.get(i), new TMapData.FindTitlePOIListenerCallback()
+            {
                 @Override
-                public void run() {
-                    Toast.makeText(MapActivity.this, address, Toast.LENGTH_SHORT).show();
+                public void onFindTitlePOI(ArrayList<TMapPOIItem> arrayList)
+                {
+                    for(int j = 0; j < arrayList.size(); j++ )
+                    {
+                        TMapPOIItem tMapPOIItem = arrayList.get(j);
+                        arrTMapPoint.add(tMapPOIItem.getPOIPoint());
+                        arrTitle.add(tMapPOIItem.getPOIName());
+                        arrAddress.add(tMapPOIItem.upperAddrName + " " +
+                                tMapPOIItem.middleAddrName + " " + tMapPOIItem.lowerAddrName);
+                    }
+                    setMultiMarkers(arrTMapPoint, arrTitle, arrAddress);
                 }
             });
-        }catch(Exception e) {
-            e.printStackTrace();
+        }
+    }
+
+    // 마커 설정
+    private void setMultiMarkers(ArrayList<TMapPoint> arrTPoint, ArrayList<String> arrTitle,
+                                 ArrayList<String> arrAddress)
+    {
+        for( int i = 0; i < arrTPoint.size(); i++ )
+        {
+            Bitmap bitmapIcon = createMarkerIcon(R.drawable.poi_dot);
+
+            TMapMarkerItem tMapMarkerItem = new TMapMarkerItem();
+            tMapMarkerItem.setIcon(bitmapIcon);
+
+            tMapMarkerItem.setTMapPoint(arrTPoint.get(i));
+
+            tmapview.addMarkerItem("markerItem" + i, tMapMarkerItem);
+
+            setBalloonView(tMapMarkerItem, arrTitle.get(i), arrAddress.get(i));
+        }
+    }
+
+    // 풍선뷰
+    private void setBalloonView(TMapMarkerItem marker, String title, String address)
+    {
+        marker.setCanShowCallout(true);
+
+        if( marker.getCanShowCallout() )
+        {
+            marker.setCalloutTitle(title);
+            marker.setCalloutSubTitle(address);
+
+//            Bitmap bitmap = createMarkerIcon(R.drawable.right_arrow);
+//            marker.setCalloutRightButtonImage(bitmap);
+        }
+    }
+
+    private Bitmap createMarkerIcon(int image)
+    {
+        Log.e("MapViewActivity", "(F)   createMarkerIcon()");
+
+        Bitmap bitmap = BitmapFactory.decodeResource(getApplicationContext().getResources(), image);
+        bitmap = Bitmap.createScaledBitmap(bitmap, 50, 50,false);
+
+        return bitmap;
+    }
+
+    // TODO: 지도 이벤트 설정하기
+    TMapView.OnClickListenerCallback mOnClickListenerCallback = new TMapView.OnClickListenerCallback() {
+        @Override
+        public boolean onPressEvent(ArrayList<TMapMarkerItem> arrayList, ArrayList<TMapPOIItem> arrayList1, TMapPoint tMapPoint, PointF pointF) {
+            double latitude = tMapPoint.getLatitude();
+            double longitude = tMapPoint.getLongitude();
+
+//                Toast.makeText(MapActivity.this, "onPressed~!", Toast.LENGTH_SHORT).show();
+            return false;
         }
 
-        //지도 이벤트 설정하기
-        tmapview.setOnClickListenerCallBack(new TMapView.OnClickListenerCallback() {
-            @Override
-            public boolean onPressEvent(ArrayList<TMapMarkerItem> arrayList, ArrayList<TMapPOIItem> arrayList1, TMapPoint tMapPoint, PointF pointF) {
-//                Toast.makeText(MapActivity.this, "onPress~!", Toast.LENGTH_SHORT).show();
-                return false;
-            }
-
-            @Override
-            public boolean onPressUpEvent(ArrayList<TMapMarkerItem> arrayList, ArrayList<TMapPOIItem> arrayList1, TMapPoint tMapPoint, PointF pointF) {
+        @Override
+        public boolean onPressUpEvent(ArrayList<TMapMarkerItem> arrayList, ArrayList<TMapPOIItem> arrayList1, TMapPoint tMapPoint, PointF pointF) {
 //                Toast.makeText(MapActivity.this, "onPressUp~!", Toast.LENGTH_SHORT).show();
-                return false;
-            }
-        });
+            return false;
+        }
+    };
+
+
+
 
 
 /*        // TODO: 위치 넘겨서 각 카드뷰에 지도 띄우기 switch문 말고~
@@ -96,5 +173,4 @@ public class MapActivity extends AppCompatActivity {
                 //추가 하면됨
                 break;
         }*/
-    }
 }
