4cbbdade99ce759ce3d89f4b93a805c4eddc6cd4
Lee-do-yeong <dlehdud98@nate.com>
2021-06-12 13:58:59 +0900

    편집 구현

--- MyCalendar/app/src/main/java/org/techtown/mycalendar/MainActivity.java
+++ MyCalendar/app/src/main/java/org/techtown/mycalendar/MainActivity.java
@@ -86,16 +86,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             @Override
             public void onLeftClicked(int position){
                 //TODO:장소 변경방법 생각... 일자시간 변경...
-                /*String location = list.get(position).getLocation();
+                String location = list.get(position).getLocation();
                 int uid = list.get(position).getUid();
-
                 userRepository.delete(list.get(position).getUid());
+                adapter.datas.remove(position);
                 adapter.notifyItemRemoved(position);
                 adapter.notifyItemRangeChanged(position, adapter.getItemCount());
                 intent.putExtra("location", location);
                 intent.putExtra("uid", uid);
                 intent = new Intent(getApplicationContext(), AddscheduleActivity.class);
-                startActivity(intent);*/
+                startActivity(intent);
             }
         });
         ItemTouchHelper itemTouchhelper = new ItemTouchHelper(swipeController);
